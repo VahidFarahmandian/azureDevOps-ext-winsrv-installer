d2778e582b676f65b69a6df7454a75c6e433d0d6
Vahid Farahmandian <farahmandian2011@gmail.com>
2023-02-21 21:07:56 +0330

    init

---
+++ source/extension/Task/InstallWindowsService.ps1
@@ -0,0 +1,74 @@
+﻿param(
+    [Parameter(Mandatory = $True)]
+    [String]
+    $serviceName,
+   
+    [Parameter(Mandatory = $True)]
+    [String]
+    $service_binaryPath,
+    
+    [Parameter(Mandatory = $True)]
+    [String]
+    $service_identity_username,
+    
+    [Parameter(Mandatory = $False)]
+    [String]
+    $service_identity_password="",
+    
+    [Parameter(Mandatory = $False)]
+    [String]
+    $serviceFriendlyName="",
+   
+    
+    [Parameter(Mandatory = $False)]
+    [String]
+    $service_startupType = "Automatic"
+
+)
+if($serviceFriendlyName -eq "")
+{
+    $serviceFriendlyName = $serviceName
+}
+$service = Get-Service -Name $($serviceName) -ErrorAction SilentlyContinue
+
+if ($service.Length -gt 0) {
+    Stop-Service -Name "$($serviceName)" -Force
+    Write-Host "Service stopping..."
+    (Get-Service $serviceName).WaitForStatus('Stopped')
+    Write-Host "Service stopped..."
+    $pidnumber  = (get-wmiobject win32_service | where { $_.name -eq $($serviceName)}).processID
+    if($pidnumber -gt 0)
+    {
+        Write-Host "Service PID: $($pidnumber) is alive!"
+        Stop-Process $pidnumber -Force
+        Write-Host "Service PID: $($pidnumber) killed"
+    }
+    Write-Host "Removing service..."
+    ##Remove-Service -Name "$($serviceName)"
+    sc.exe delete "$($serviceName)"
+    Write-Host "Service removed..."
+}
+Write-Host "Creating service..."
+$username = "$($service_identity_username)"
+$password = "$($service_identity_password)"
+$securepassword=""
+if ($password -eq "") 
+{
+    $securepassword = (new-object System.Security.SecureString)
+}
+else
+{
+    $securepassword = ConvertTo-SecureString $password -AsPlainText -Force
+}
+$cred = New-Object System.Management.Automation.PSCredential ($username, $securepassword)
+$params = @{
+  Name = "$($serviceName)"
+  BinaryPathName = "$($service_binaryPath)"
+  DisplayName = "$($serviceFriendlyName)"
+  StartupType = "$($service_startupType)"
+}
+New-Service @params -Credential $cred 
+Write-Host "Service created."
+Write-Host "Starting service..."
+Start-Service -Name "$($serviceName)"
+Write-Host "Service Installation done!"
---
+++ source/sample win srv/windows service/SampleService/SampleService/Service1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace SampleService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        Timer timer = new Timer();
+        protected override void OnStart(string[] args)
+        {
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000; //number in milisecinds  
+            timer.Enabled = true;
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+        }
+        protected override void OnStop()
+        {
+        }
+    }
+}
